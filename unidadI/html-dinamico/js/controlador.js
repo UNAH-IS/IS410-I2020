ef6c54893cee2e4a1f08febe0804bc3648979d0a
Erick Marín <erick.marin.v@gmail.com>
2020-02-24 20:52:21 -0600

    Javascript DOM

---
+++ unidadI/html-dinamico/js/controlador.js
@@ -0,0 +1,88 @@
+let artistas = [
+    {
+        nombreArtista:'Metallica',
+        caratulaArtista: 'img/1.jpg',
+        albumes:[
+            {
+                tituloAlbum:'Album 1',
+                anio:2222,
+                genero:'Rock',
+                caratula:'img/1.jpg',
+                canciones:[
+                    {
+                        nombreCancion:'Perreo intenso',
+                        duracion:'3:33'
+                    }
+                ]
+            },
+            {
+                tituloAlbum:'Album 2',
+                anio:2222,
+                genero:'Queso',
+                caratula:'img/2.jpg',
+                canciones:[
+                    {
+                        nombreCancion:'Perreo intenso',
+                        duracion:'3:33'
+                    }
+                ]
+            },
+            {
+                tituloAlbum:'Album 3',
+                anio:2222,
+                genero:'Rock',
+                caratula:'img/3.jpg',
+                canciones:[
+                    {
+                        nombreCancion:'Perreo intenso',
+                        duracion:'3:33'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        nombreArtista:'Epica',
+        caratulaArtista: 'img/2.jpg',
+        albumes:[
+            {
+                tituloAlbum:'Album 1',
+                anio:2222,
+                genero:'Rock',
+                caratula:'img/1.jpg',
+                canciones:[
+                    {
+                        nombreCancion:'Tusa',
+                        duracion:'3:33'
+                    }
+                ]
+            },
+            {
+                tituloAlbum:'Album 2',
+                anio:2222,
+                genero:'Queso',
+                caratula:'img/2.jpg',
+                canciones:[
+                    {
+                        nombreCancion:'Otra rola',
+                        duracion:'3:33'
+                    }
+                ]
+            },
+            {
+                tituloAlbum:'Album 3',
+                anio:2222,
+                genero:'Rock',
+                caratula:'img/3.jpg',
+                canciones:[
+                    {
+                        nombreCancion:'Perreo intenso',
+                        duracion:'3:33'
+                    }
+                ]
+            }
+        ]
+    }
+];
+
+console.log(artistas);
